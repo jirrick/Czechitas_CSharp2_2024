a9a9d83616cd69a1c3ba3cbca81cde08fe796b3a
Jiri Hudec <jurahudec@gmail.com>
2024-04-11 19:36:19 +0200

    Upraveny pozdrav

--- Lekce1_Opakovani/Program.cs
+++ Lekce1_Opakovani/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Vitejte na kurzu C# 2!");
         }
     }
 }
